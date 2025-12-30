ace5b6da9704a5657e5943cb60d33a9c5c8e9116
Chris Cheng <chrischeng@Chriss-Mac-mini-M4-Pro.local>
2025-12-30 15:00:01 +0800

    Setup for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // CHANGE THIS: Match your GitHub repository name
-  base: "/sg-hr-assistant/", 
+  base: "/sg-hr-assistant/",
 })
